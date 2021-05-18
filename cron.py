0a125d177ed41666dcf3d6a4cff93c5121dbadcb
Nathan <nshuffman@gmail.com>
2021-05-18 18:55:41 -0400

    Update to test_log files

--- cron.py
+++ cron.py
@@ -2,6 +2,6 @@ from crontab import CronTab
 
 cron = CronTab(user=True)
 job = cron.new(command='cd /home/nathan/RPi_Projects && $(which python3) /home/nathan/RPi_Projects/main.py')
-job.minute.every(5)
+job.minute.every(10)
 
 cron.write()
